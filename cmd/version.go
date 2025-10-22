3e81ef38e37db1757f482c740d0245756a625140
MingLoong <52956168+MingLoong@users.noreply.github.com>
2025-10-22 14:42:11 +0800

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -8,14 +8,14 @@ import (
 
 var (
 	version  = "0.9.6"        // 版本递增
-	codename = "MyXR"         // 修改 codename
-	intro    = "A MyXR backend that supports many panels" // 修改介绍文字
+	codename = "MR"           // 修改 codename
+	intro    = "A MR backend that supports many panels" // 修改介绍文字
 )
 
 func init() {
 	rootCmd.AddCommand(&cobra.Command{
 		Use:   "version",
-		Short: "Print current version of MyXR",
+		Short: "Print current version of MR",
 		Run: func(cmd *cobra.Command, args []string) {
 			showVersion()
 		},
